523788802ffa884ef4a5235293bc95efb5bfd008
Valerii Kolomiiets <valerii.kolomiets@ekreative.com>
2020-11-15 18:05:25 +0200

    test origin

--- src/todo/todo.repository.ts
+++ src/todo/todo.repository.ts
@@ -3,6 +3,7 @@ import { Todo } from "./todo.entity";
 
 @EntityRepository(Todo)
 export class TodoRepository extends Repository<Todo> {
+  
   async findById(id): Promise<Todo> {
     return this.findOne(id, { relations: ['settings'] });
   }
